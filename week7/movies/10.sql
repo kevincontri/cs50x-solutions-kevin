b8560b05539b43e705a490d9f703497f4669b963
Kevin Contri <kevin.lucas.contri@gmail.com>
2026-01-21 11:41:28 -0300

    Update 10.sql

--- week7/movies/10.sql
+++ week7/movies/10.sql
@@ -1,2 +1,11 @@
-SELECT name FROM people WHERE people.id IN (SELECT person_id FROM directors JOIN movies ON movies.id = directors.movie_id JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating >= 9.0);
+SELECT name 
+  FROM people 
+  WHERE people.id IN 
+  (SELECT person_id 
+  FROM directors 
+  JOIN movies 
+  ON movies.id = directors.movie_id 
+  JOIN ratings 
+  ON movies.id = ratings.movie_id 
+  WHERE ratings.rating >= 9.0);
 
